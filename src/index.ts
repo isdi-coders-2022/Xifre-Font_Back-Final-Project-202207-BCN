5513ba71f78365e942029ea06a3859e3cb5dd02f
Xifre Font <xifre.font.97@gmail.com>
2022-08-29 15:28:21 +0200

    Installed cors and morgan and using them at index

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,15 @@ import "./loadEnvironment";
 import express from "express";
 import Debug from "debug";
 import chalk from "chalk";
+import cors from "cors";
+import morgan from "morgan";
 
 const debug = Debug("widescope:index");
 
 const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
 
 app.listen(4000, () => {
   debug(chalk.green("Server listening on port 4000"));
