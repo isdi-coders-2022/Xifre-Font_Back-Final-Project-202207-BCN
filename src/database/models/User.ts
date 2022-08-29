eb1de9527c4c16499848a222b9798c5d038cb232
Xifre Font <xifre.font.97@gmail.com>
2022-08-29 18:32:37 +0200

    Hotfixed documents not deleting the default id and version control

--- src/database/models/User.ts
+++ src/database/models/User.ts
@@ -20,17 +20,4 @@ export const userSchema = new Schema<IUser>({
   contacts: [String],
 });
 
-userSchema.set("toJSON", {
-  virtuals: true,
-  transform: (doc, ret) => {
-    const newDocument = { ...ret };
-    delete newDocument.password;
-    // eslint-disable-next-line no-underscore-dangle
-    delete newDocument.__v;
-    // eslint-disable-next-line no-underscore-dangle
-    delete newDocument._id;
-    return newDocument;
-  },
-});
-
 export const User = model("User", userSchema, "users");
