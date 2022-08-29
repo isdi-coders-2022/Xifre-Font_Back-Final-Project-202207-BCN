57a599c3b5a15fc88b390e5034fd722568322936
Xifre Font <xifre.font.97@gmail.com>
2022-08-29 15:48:47 +0200

    Installed mongoose and created a function to connect to database

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,17 @@ import "./loadEnvironment";
 import Debug from "debug";
 import chalk from "chalk";
 import startServer from "./server/startServer";
+import connectDB from "./database";
 
 const debug = Debug("widescope:index");
 
 const port = +process.env.PORT ?? 4000;
+const database = process.env.MONGO_DB;
 
 (async () => {
   debug(chalk.gray("Starting server and connecting to the database"));
   try {
+    await connectDB(database);
     await startServer(port);
   } catch (error) {
     debug(chalk.red("Error while starting the server and the database"));
