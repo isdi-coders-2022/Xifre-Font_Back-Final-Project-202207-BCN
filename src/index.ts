a33dc9f926e9dfde125863f505d0bfe44c31ab9d
Xifre Font <xifre.font.97@gmail.com>
2022-08-29 15:39:55 +0200

    Start the server from index and changed sonar exclusions

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import "./loadEnvironment";
-import express from "express";
 import Debug from "debug";
 import chalk from "chalk";
-import cors from "cors";
-import morgan from "morgan";
+import startServer from "./server/startServer";
 
 const debug = Debug("widescope:index");
 
-const app = express();
+const port = +process.env.PORT ?? 4000;
 
-app.use(cors());
-app.use(morgan("dev"));
-app.disable("x-powered-by");
-
-app.listen(4000, () => {
-  debug(chalk.green("Server listening on port 4000"));
-});
+(async () => {
+  debug(chalk.gray("Starting server and connecting to the database"));
+  try {
+    await startServer(port);
+  } catch (error) {
+    debug(chalk.red("Error while starting the server and the database"));
+    process.exit(1);
+  }
+})();
