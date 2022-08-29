e0b53b2b23b28ce8998448238b3bd2be593a169b
Xifre Font <xifre.font.97@gmail.com>
2022-08-29 17:34:49 +0200

    Created and tested function to create tokens with jwt

--- src/utils/auth/auth.test.ts
+++ src/utils/auth/auth.test.ts
@@ -1,4 +1,5 @@
-import createHash from "./auth";
+import Payload from "../../types/Payload";
+import { createToken, hashCreate } from "./auth";
 
 describe("Given a hashCreate function", () => {
   describe("When instantiated with a string as an argument", () => {
@@ -6,10 +7,26 @@ describe("Given a hashCreate function", () => {
       const password = "admin";
       const hashStart = "$2a$10$";
 
-      const result = await createHash(password);
+      const result = await hashCreate(password);
 
       expect(result.startsWith(hashStart)).toBe(true);
       expect(result.length > 10).toBe(true);
+    });
+  });
+});
+
+describe("Given a createToken function", () => {
+  describe("When called with a payload as an argument", () => {
+    test("Then it should return a signed token", () => {
+      const minExpectedSignLength = 20;
+      const mockToken: Payload = {
+        id: "1234",
+        name: "aaa",
+      };
+
+      const result = createToken(mockToken);
+
+      expect(result.length > minExpectedSignLength).toBe(true);
     });
   });
 });
--- src/utils/auth/auth.ts
+++ src/utils/auth/auth.ts
@@ -1,8 +1,11 @@
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import Payload from "../../types/Payload";
 
 export const hashCreate = (password: string): Promise<string> => {
   const salt = 10;
   return bcrypt.hash(password, salt);
 };
 
-export default hashCreate;
+export const createToken = (payload: Payload): string =>
+  jwt.sign(payload, process.env.AUTH_SECRET || "temporal_fix");
