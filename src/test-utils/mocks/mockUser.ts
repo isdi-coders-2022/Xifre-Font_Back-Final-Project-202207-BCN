c28f6f5065e86830ad6ef45d65ac8728d6fd7fb2
Xifre Font <xifre.font.97@gmail.com>
2022-09-05 23:41:28 +0200

    Tested the project router for the delete endpoint

--- src/test-utils/mocks/mockUser.ts
+++ src/test-utils/mocks/mockUser.ts
@@ -2,7 +2,7 @@ import IUser from "../../database/types/IUser";
 
 const mockUser: IUser = {
   id: "6305fe53bvf81v5baop3e52d",
-  name: "longusername",
+  name: "John Doe",
   password: "longpassword123",
   email: "user@email.com",
   contacts: ["contact_one", "contact_two"],
