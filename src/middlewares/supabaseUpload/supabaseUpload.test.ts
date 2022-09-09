ae2ff86b86954a106b054a32be9e54d3b9c4b212
Xifre Font <xifre.font.97@gmail.com>
2022-09-09 17:06:57 +0200

    Tested the supabase middleware

---
+++ src/middlewares/supabaseUpload/supabaseUpload.test.ts
@@ -0,0 +1,108 @@
+import { NextFunction, Request, Response } from "express";
+import fs from "fs/promises";
+import codes from "../../configs/codes";
+import CreateError from "../../utils/CreateError/CreateError";
+import supabaseUpload from "./supabaseUpload";
+
+let mockUpload = jest.fn().mockReturnValue({
+  error: false,
+});
+
+jest.mock("@supabase/supabase-js", () => ({
+  createClient: () => ({
+    storage: {
+      from: () => ({
+        upload: mockUpload,
+        getPublicUrl: () => ({
+          publicURL: "Image url",
+        }),
+      }),
+    },
+  }),
+}));
+
+describe("Given a supabaseUpload function", () => {
+  beforeAll(async () => {
+    await fs.writeFile("public/uploads/logo-name.png", "content");
+  });
+
+  afterAll(async () => {
+    await fs.unlink("public/uploads/logo-name.png");
+  });
+
+  const req = {
+    body: {
+      logo: "logo-name.png",
+    },
+  } as Partial<Request>;
+  const res = {} as Partial<Response>;
+  const next = jest.fn() as NextFunction;
+
+  describe("When called with a request, a response and a next function as arguments", () => {
+    test("Then it should call next", async () => {
+      await supabaseUpload(
+        req as Request,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(next).toHaveBeenCalled();
+    });
+
+    test("Then it should upload a file to supabase", async () => {
+      await supabaseUpload(
+        req as Request,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(mockUpload).toHaveBeenCalled();
+    });
+
+    test("If the upload fails, it should call next with an error", async () => {
+      mockUpload = jest.fn().mockReturnValue({
+        error: true,
+      });
+
+      await supabaseUpload(
+        req as Request,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(next).toHaveBeenCalledWith(true);
+    });
+
+    test("Then it should set the image url at the body request", async () => {
+      await supabaseUpload(
+        req as Request,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(req.body.logoBackup).toBe("Image url");
+    });
+
+    test("If an error occurs during the process, it should call next with an error", async () => {
+      jest.clearAllMocks();
+      mockUpload = jest.fn().mockRejectedValue(new Error());
+
+      const expectedError = new CreateError(
+        codes.internalServerError,
+        "Couldn't upload or read the image",
+        "Error while reading and uploading the image"
+      );
+
+      await supabaseUpload(
+        req as Request,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+
+      const nextCalled = (next as jest.Mock<any, any>).mock.calls[0][0];
+      expect(nextCalled.privateMessage).toBe(expectedError.privateMessage);
+    });
+  });
+});
